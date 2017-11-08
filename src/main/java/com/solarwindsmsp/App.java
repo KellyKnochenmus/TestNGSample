dbf38ed544d99cb36caa34348c88c7346f4928fa
KellyKnochenmus <kelly_k@verizon.net>
2017-11-08 11:25:01 -0600

    initial commit

---
+++ src/main/java/com/solarwindsmsp/App.java
@@ -0,0 +1,14 @@
+package com.solarwindsmsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
